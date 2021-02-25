b4f62b1777d82ca1ebcfd52e0e3adb4402146fca
Ben Bergen <bergen@lanl.gov>
2021-02-25 09:05:22 -0700

    WIP: Added damped jacobi smoother

--- tutorial/standalone/poisson/app/tasks/smooth.cc
+++ tutorial/standalone/poisson/app/tasks/smooth.cc
@@ -24,3 +24,23 @@ poisson::task::smooth(mesh::accessor<ro> m,
   } // for
   // clang format on
 } // smooth
+
+void poisson::task::jacobi(mesh::accessor<ro> m,
+  field<double>::accessor<rw, ro> ua_new,
+  field<double>::accessor<ro, ro> ua,
+  field<double>::accessor<ro, ro> fa,
+  double omega) {
+  auto u_new = m.mdspan<mesh::vertices>(ua_new);
+  auto u = m.mdspan<mesh::vertices>(ua);
+  auto f = m.mdspan<mesh::vertices>(fa);
+  const auto dsqr = pow(m.delta(), 2);
+
+  // clang-format off
+  for(auto j : m.vertices<mesh::y_axis>()) {
+    for(auto i : m.vertices<mesh::x_axis>()) {
+      u_new[j][i] = (1.0 - omega) * u[j][i] + omega*(0.25 * (dsqr * f[j][i] +
+        u[j][i + 1] + u[j][i - 1] + u[j + 1][i] + u[j - 1][i]));
+    } // for
+  } // for
+  // clang format on
+} // jacobi
